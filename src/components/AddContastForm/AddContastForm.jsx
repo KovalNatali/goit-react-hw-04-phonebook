c6a4880e3d0a366b48ab800e1dbbb9ff00b1e404
KovalNatali <koval.natali.777@gmail.com>
2023-10-30 12:08:01 +0200

    hw-04-phone

--- src/components/AddContastForm/AddContastForm.jsx
+++ src/components/AddContastForm/AddContastForm.jsx
@@ -1,55 +1,50 @@
 import { TextField, Button } from "@mui/material"
 import {Form, Label} from './AddContactForm.styled'
-import { Component } from "react"
+import { useState } from "react"
 import { nanoid } from "nanoid";
 
 
-export class AddContastForm extends Component {
-    state = {
-        name: '',
-        number: '',
-      };
+export const AddContastForm = ({addContast}) => {
+    const [name, setName] =  useState('');
+    const [number, setNumber] =  useState('');
 
 
-handalCange = (evt) => {
-this.setState({
-    [evt.target.name]: evt.target.value,
-});
+const handalCange = (e) => {
+    e.target.name === 'name' && setName(e.target.value);
+    e.target.name === 'number' && setNumber(e.target.value);
+  
 };
 
-    render () {
-            return <Form onSubmit={evt => {
+    return ( <Form onSubmit={evt => {
             evt.preventDefault();
-            this.props.addContast({id: nanoid(5),...this.state});
-            this.setState({
-                name: '',
-                number: '',
-            });
+            addContast({id: nanoid(5),name, number});
+            setName('');
+            setNumber('');
             }}>
         <Label> Name</Label>
         <TextField
-         onChange={this.handalCange} 
+         onChange={handalCange} 
   id="name" 
   type="text"
   name="name"
   pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
   title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
   required label="Name" variant="outlined" 
-  value={this.state.name}/> 
+  value={name}/> 
 
   <Label> Number</Label>
 
-<TextField onChange={this.handalCange}
+<TextField onChange={handalCange}
   type="tel"
   name="number"
   pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
   title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
   required label="Number" variant="outlined"
-  value={this.state.number}
+  value={number}
 />
  <Button type="submit" variant="outlined">Add contact</Button>
         
     </Form>
-    }
+    )
 
 } 
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React, {useState,useEffect} from 'react'
 import { AddContastForm } from './AddContastForm/AddContastForm'
 import { Container } from '@mui/material'
 import { ContactsList } from './ContactsList/ContactsList'
@@ -6,36 +6,32 @@ import { Filter } from './Filter/Filter'
 import { nanoid } from "nanoid";
 
 
-export class App extends Component {
-  state = {
-    contacts: [
-    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-    ],
-    filter: ''
-  }
-  componentDidMount() {
+export const App = () =>  {
+  const [contacts, setContacts] = useState([
+      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+      ],)
+
+  const [filter, setFilter] = useState('')
+ 
+
+  useEffect(() => {
     const savedContacs = localStorage.getItem(`contacts`);
     if(savedContacs !== null) {
-      this.setState({
-        contacts: JSON.parse(savedContacs)
-      });
+    JSON.parse(savedContacs)
     }
-
-  }     
+  },[])     
  
  
- componentDidUpdate(prevPros, prevState) {
- if(this.state.contacts !== prevState.contacts) {
-  localStorage.setItem(`contacts`,JSON.stringify(this.state.contacts))
- }
- }
+ useEffect(() => {
+  localStorage.setItem(`contacts`,JSON.stringify(contacts))
+ },[contacts])
  
 
-addContact = newContact => {
-  const isExist = this.state.contacts.find(contact => {
+const addContact = newContact => {
+  const isExist = contacts.find(contact => {
     return newContact.name === contact.name;
   })
   if (isExist) {
@@ -43,39 +39,41 @@ addContact = newContact => {
     return;
   }
 
-  this.setState(prevState => ({
-    contacts: [...prevState.contacts, { id: nanoid(), ...newContact }],
-  }));
+ setContacts(prevContacts => [...prevContacts,{ id: nanoid(), ...newContact }]
+    // contacts: [...prevState.contacts, { id: nanoid(), ...newContact }],
+  );
 };
 
-handleDeleteContact = (id) => {
-  this.setState(prevState => ({contacts: prevState.contacts.filter((contact) => contact.id!== id) }))
+const handleDeleteContact = (id) => {
+  setContacts(contacts.filter((contact) => contact.id!== id))
+  // this.setState(prevState => ({contacts: prevState.contacts.filter((contact) => contact.id!== id) }))
 }
 
-handleChange = (evt) => {
-  this.setState(prevState => ({filter: evt.target.value}));
+const handleChange = (evt) => {
+  setFilter(evt.target.value)
+  // this.setState(prevState => ({filter: evt.target.value}));
 }
 
-getFilterContacts = () => {
-  const normalizedFilter = this.state.filter.toLowerCase().trim();
-  return this.state.contacts.filter((contact) => {
+const getFilterContacts = () => {
+  const normalizedFilter = filter.toLowerCase().trim();
+  return contacts.filter((contact) => {
     return contact.name.toLowerCase().includes(normalizedFilter)
 
   });
 }
-  render() {
+  
       return (
     < Container maxWidth="xl" >
         <h1>Phonebook</h1>
-<AddContastForm addContast={this.addContact} contactsList={this.contacts}/>
+<AddContastForm addContast={addContact} contactsList={contacts}/>
 <h2>Contacts</h2>   
-     <Filter handleChange={this.handleChange}/>
+     <Filter handleChange={handleChange}/>
      <ContactsList 
-     contacts={this.getFilterContacts()}
-     onDeleteContact={this.handleDeleteContact}/>
+     contacts={getFilterContacts()}
+     onDeleteContact={handleDeleteContact}/>
   
     </ Container >
-  )};
+  );
 
 }
 
